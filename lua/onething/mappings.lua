01555a753ea8a62570465a15f93d06e5f38dc28b
Marc Vincent Bentoy <marc.bentoy@gmail.com>
2024-12-12 11:36:40 +0800

    remap toggle

--- lua/onething/mappings.lua
+++ lua/onething/mappings.lua
@@ -2,7 +2,7 @@ local M = {}
 
 M.setup = function()
     vim.keymap.set("n", "<leader>otu", ":UpdateOnething ", { desc = "Update onething focus phrase" })
-    vim.keymap.set("n", "<leader>otx", ":ToggleOnethingVisibilty<CR>", { desc = "Toggles onething visibility" })
+    vim.keymap.set("n", "<leader>ots", ":ToggleOnethingVisibilty<CR>", { desc = "Toggles onething visibility" })
 end
 
 return M
