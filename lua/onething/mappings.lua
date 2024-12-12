9429a02b996578189204d7d2c23957c6d500bedd
Marc Vincent Bentoy <marc.bentoy@gmail.com>
2024-12-12 11:33:04 +0800

    fix: keymapping

--- lua/onething/mappings.lua
+++ lua/onething/mappings.lua
@@ -2,7 +2,7 @@ local M = {}
 
 M.setup = function()
     vim.keymap.set("n", "<leader>otu", ":UpdateOnething ", { desc = "Update onething focus phrase" })
-    vim.keymap.set("n", "<leader>otx", ":UpdateOnething ", { desc = "Update onething focus phrase" })
+    vim.keymap.set("n", "<leader>otx", ":ToggleOnethingVisibilty<CR>", { desc = "Toggles onething visibility" })
 end
 
 return M
