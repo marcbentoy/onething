a1a81f26a7ea1e0972f0f98c1d678d63054e019c
Marc Vincent Bentoy <marc.bentoy@gmail.com>
2024-12-12 11:30:35 +0800

    toggle window

--- lua/onething/commands.lua
+++ lua/onething/commands.lua
@@ -2,8 +2,18 @@ local M = {}
 
 local phrasebuf = nil
 local phrasewin = nil
+local show = false
+local lphrase = ""
+
+local function close_window() 
+    if phrasewin and vim.api.nvim_win_is_valid(phrasewin) then
+        vim.api.nvim_win_close(phrasewin, true)
+    end
+    show = false
+end
 
 M.create_update_floating_window = function(phrase)
+    lphrase = phrase
     local winconf = {
         relative = "editor",
         width = #phrase,
@@ -13,6 +23,8 @@ M.create_update_floating_window = function(phrase)
         style = "minimal",
         border = "rounded",
     }
+
+    show = true
 
     if phrasebuf and vim.api.nvim_buf_is_valid(phrasebuf) and phrasewin and vim.api.nvim_win_is_valid(phrasewin) then
         vim.api.nvim_buf_set_lines(phrasebuf, 0, -1, false, { phrase })
@@ -28,6 +40,15 @@ M.create_update_floating_window = function(phrase)
     phrasewin = vim.api.nvim_open_win(phrasebuf, false, winconf)
 end
 
+M.toggle_visibility = function()
+    if show then
+        close_window()
+    else
+        M.create_update_floating_window(lphrase)
+    end
+end
+
+
 M.setup = function()
     vim.api.nvim_create_user_command(
         "UpdateOnething", function(opts)
@@ -35,6 +56,13 @@ M.setup = function()
             M.create_update_floating_window(phrase)
         end,
         { nargs = 1, desc = "Update onething phrase" }
+    )
+
+    vim.api.nvim_create_user_command(
+        "ToggleOnethingVisibilty", function()
+            M.toggle_visibility()
+        end,
+        { nargs = 1, desc = "Show or hide onething window" }
     )
 end
 
--- lua/onething/mappings.lua
+++ lua/onething/mappings.lua
@@ -2,6 +2,7 @@ local M = {}
 
 M.setup = function()
     vim.keymap.set("n", "<leader>otu", ":UpdateOnething ", { desc = "Update onething focus phrase" })
+    vim.keymap.set("n", "<leader>otx", ":UpdateOnething ", { desc = "Update onething focus phrase" })
 end
 
 return M
